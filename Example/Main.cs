6067cab48e51921d8d47b256edde47d9eebdf623
Nasr <larko236@outlook.com>
2023-11-24 10:32:07 -0500

    feat: toriiclient c# wrapper

--- Example/Main.cs
+++ Example/Main.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using bottlenoselabs.C2CS.Runtime;
 using dojo_namespace;
 
@@ -12,31 +13,111 @@ namespace Dojo
         {
             CString ctoriiUrl = CString.FromString(toriiUrl);
             CString crpcUrl = CString.FromString(rpcUrl);
-            unsafe
+            dojo.EntityQuery* entitiesPtr;
+
+            fixed (dojo.EntityQuery* ptr = &entities[0])
             {
-                dojo.EntityQuery* entitiesPtr;
+                entitiesPtr = ptr;
+            }
 
-                fixed (dojo.EntityQuery* ptr = &entities[0])
-                {
-                    entitiesPtr = ptr;
-                }
+            dojo.Error* error = null;
+            client = dojo.client_new(ctoriiUrl, crpcUrl, &world, entitiesPtr, (nuint)entities.Length, error);
 
-                dojo.Error* error = null;
-                this.client = dojo.client_new(ctoriiUrl, crpcUrl, &world, entitiesPtr, (nuint)entities.Length, error);
+            if (error != null)
+            {
+                throw new Exception(error->message);
             }
         }
 
         ~ToriiClient()
         {
-            dojo.client_free(this.client);
+            dojo.client_free(client);
+        }
+
+        public dojo.WorldMetadata WorldMetadata()
+        {
+            dojo.WorldMetadata worldMetadata = dojo.client_metadata(client);
+
+            return worldMetadata;
         }
 
         public dojo.Ty Entity(dojo.EntityQuery query)
         {
             dojo.Error* error = null;
-            dojo.Ty entity = *dojo.client_entity(this.client, &query, error);
+            dojo.Ty entity = *dojo.client_entity(client, &query, error);
+
+            if (error != null)
+            {
+                throw new Exception(error->message);
+            }
 
             return entity;
+        }
+
+        public ReadOnlySpan<dojo.EntityQuery> Entities(dojo.EntityQuery query)
+        {
+            dojo.CArray_EntityQuery* entities = dojo.client_subscribed_entities(client);
+
+
+            return new Span<dojo.EntityQuery>(entities->data, (int)entities->data_len);
+        }
+
+        public void AddEntitiesToSync(dojo.EntityQuery[] entities)
+        {
+            dojo.EntityQuery* entitiesPtr;
+
+            fixed (dojo.EntityQuery* ptr = &entities[0])
+            {
+                entitiesPtr = ptr;
+            }
+
+            dojo.Error* error = null;
+            dojo.client_add_entities_to_sync(client, entitiesPtr, (nuint)entities.Length, error);
+
+            if (error != null)
+            {
+                throw new Exception(error->message);
+            }
+        }
+
+        public void RemoveEntitiesToSync(dojo.EntityQuery[] entities)
+        {
+            dojo.EntityQuery* entitiesPtr;
+
+            fixed (dojo.EntityQuery* ptr = &entities[0])
+            {
+                entitiesPtr = ptr;
+            }
+
+            dojo.Error* error = null;
+            dojo.client_remove_entities_to_sync(client, entitiesPtr, (nuint)entities.Length, error);
+
+            if (error != null)
+            {
+                throw new Exception(error->message);
+            }
+        }
+
+        public void OnEntityStateUpdate(dojo.EntityQuery query, dojo.FnPtr_Void callback)
+        {
+            dojo.Error* error = null;
+            dojo.client_on_entity_state_update(client, &query, callback, error);
+
+            if (error != null)
+            {
+                throw new Exception(error->message);
+            }
+        }
+
+        public void StartSubscription()
+        {
+            dojo.Error* error = null;
+            dojo.client_start_subscription(client, error);
+
+            if (error != null)
+            {
+                throw new Exception(error->message);
+            }
         }
     }
 }
