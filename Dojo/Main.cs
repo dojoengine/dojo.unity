0d2e64bac13604ebd2857aff738ace246cc7cf36
Nasr <larko236@outlook.com>
2023-11-24 12:39:26 -0500

    feat: example

--- Dojo/Main.cs
+++ Dojo/Main.cs
@@ -1,13 +1,46 @@
+using System.Runtime.CompilerServices;
 using bottlenoselabs.C2CS.Runtime;
 using Dojo;
 using dojo_bindings;
 
 static class Example
 {
+    static void OnEntityStateUpdate()
+    {
+        // React
+    }
+
     static void Main()
     {
-        ToriiClient client = new ToriiClient("http://localhost:1234", "http://localhost:1234", new dojo.FieldElement { }, new dojo.EntityQuery[]{new dojo.EntityQuery{
-            _model=CString.FromString("")
-        }});
+        // Initialize world data
+        var world = new dojo.FieldElement { };
+        // Initialize entities
+        var entities = new dojo.EntityQuery[] { new dojo.EntityQuery { _model = CString.FromString("") } };
+
+        // Throws an exception if the client cannot be created
+        ToriiClient client = new ToriiClient("http://localhost:1234", "http://localhost:1234", world, entities);
+
+        // Add our entities to the sync list
+        client.AddEntitiesToSync(entities);
+
+        // Listen for updates
+        client.OnEntityStateUpdate(new dojo.EntityQuery
+        {
+            _model = CString.FromString(""),
+            clause = new dojo.Clause
+            {
+                attribute = new dojo.AttributeClause
+                {
+                    _attribute = CString.FromString("name"),
+                    operator_ = dojo.ComparisonOperator.Eq,
+                    value = new dojo.Value
+                    {
+                        _v_string = CString.FromString("mimi")
+                    }
+                }
+            }
+        }, Example.OnEntityStateUpdate);
+
+        client.RemoveEntitiesToSync(entities);
     }
 }
