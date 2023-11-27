93f0563305edd00a771fc5b3aacfc3c0caefd124
Nasr <larko236@outlook.com>
2023-11-27 12:53:28 -0500

    feat: working on example and tests issues

--- DojoTests/UnitTest1.cs
+++ DojoTests/UnitTest1.cs
@@ -11,15 +11,93 @@ public class Tests
     [SetUp]
     public void Setup()
     {
+        var toriiUrl = "http://0.0.0.0:8080";
+        var rpcUrl = "http://0.0.0.0:5050";
         var world = new dojo.FieldElement { };
         var entities = new dojo.EntityQuery[] { new dojo.EntityQuery { _model = CString.FromString("") } };
         
-        client = new ToriiClient("http://localhost:50051", "http://localhost:50052", world, entities);
+        client = new ToriiClient(toriiUrl, rpcUrl, world, entities);
+
+        if (client == null)
+        {
+            throw new Exception("client is null");
+        }
     }
 
     [Test]
-    public void Test1()
+    public void TestWorldMetadata()
     {
-        Assert.Pass();
+        var worldAddress = new dojo.FieldElement { };
+
+        var worldMetadata = client.WorldMetadata();
+
+        Assert.That(worldAddress, Is.EqualTo(worldMetadata.world_address));
+        // Check other fields
+    }
+
+    [Test]
+    public void TestEntity()
+    {
+        var query = new dojo.EntityQuery
+        {
+            _model = CString.FromString(""),
+            clause = new dojo.Clause
+            {
+                attribute = new dojo.AttributeClause
+                {
+                    _attribute = CString.FromString("name"),
+                    operator_ = dojo.ComparisonOperator.Eq,
+                    value = new dojo.Value
+                    {
+                        _v_string = CString.FromString("mimi")
+                    }
+                }
+            }
+        };
+
+        var entity = client.Entity(query);
+    }
+
+    [Test]
+    public void TestAddEntitiesToSync()
+    {
+        var entities = new dojo.EntityQuery[] { new dojo.EntityQuery { _model = CString.FromString("") } };
+
+        client.AddEntitiesToSync(entities);
+
+        //TODO: Check that the entities are in the sync list
+        
+    }
+
+    [Test]
+    public void TestRemoveEntitiesToSync()
+    {
+        var entities = new dojo.EntityQuery[] { new dojo.EntityQuery { _model = CString.FromString("") } };
+
+        client.RemoveEntitiesToSync(entities);
+    }
+
+    [Test]
+    public void TestOnEntityStateUpdate()
+    {
+        dojo.FnPtr_Void.@delegate callback = () => {
+            
+        };
+        client.OnEntityStateUpdate(new dojo.EntityQuery
+        {
+            _model = CString.FromString(""),
+            clause = new dojo.Clause
+            {
+                attribute = new dojo.AttributeClause
+                {
+                    _attribute = CString.FromString("name"),
+                    operator_ = dojo.ComparisonOperator.Eq,
+                    value = new dojo.Value
+                    {
+                        _v_string = CString.FromString("mimi")
+                    }
+                }
+            }
+        }, new dojo.FnPtr_Void(callback));
     }
 }
---
+++ Example/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.CompilerServices;
+using bottlenoselabs.C2CS.Runtime;
+using Dojo;
+using dojo_bindings;
+
+internal class Example
+{
+    private static void OnEntityStateUpdate()
+    {
+        // React
+    }
+
+    unsafe static void Main()
+    {
+        // Initialize world data
+        var data = new byte[32];
+        dojo.FieldElement world;
+        fixed (byte* ptr = &data[0])
+        {
+            world = new dojo.FieldElement { _data = ptr };
+        }
+        // Initialize entities
+        var entities = new dojo.EntityQuery[] { new dojo.EntityQuery { _model = CString.FromString("Moves") } };
+
+        // Throws an exception if the client cannot be created
+        ToriiClient client = new ToriiClient("http://localhost:22", "http://localhost:1010", world, entities);
+        
+        // Get the world metadata
+        dojo.WorldMetadata worldMetadata = client.WorldMetadata();
+
+        // Add our entities to the sync list
+        client.AddEntitiesToSync(entities);
+
+        // Listen for updatest
+        dojo.FnPtr_Void.@delegate callback = OnEntityStateUpdate;
+        client.OnEntityStateUpdate(new dojo.EntityQuery
+        {
+            _model = CString.FromString(""),
+            clause = new dojo.Clause
+            {
+                attribute = new dojo.AttributeClause
+                {
+                    _attribute = CString.FromString("name"),
+                    operator_ = dojo.ComparisonOperator.Eq,
+                    value = new dojo.Value
+                    {
+                        _v_string = CString.FromString("mimi")
+                    }
+                }
+            }
+        }, new dojo.FnPtr_Void(callback));
+
+        client.RemoveEntitiesToSync(entities);
+    }
+}
