e4f874520b0289a291705c5a09a931aa59de1671
Nasr <larko236@outlook.com>
2023-12-19 14:52:19 -0500

    refactor: new submodle / bindings structure

--- Scripts/bindgen.sh
+++ Scripts/bindgen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-castffi extract --config Packages/Dojo/Runtime/bindings/config-extract-linux.json
-castffi extract --config Packages/Dojo/Runtime/bindings/config-extract-macos.json
-castffi extract --config Packages/Dojo/Runtime/bindings/config-extract-windows.json
+castffi extract --config Bindings/config-extract-linux.json
+castffi extract --config Bindings/config-extract-macos.json
+castffi extract --config Bindings/config-extract-windows.json
 
 # Merge platform abstract syntax tree .json files into a cross-platform abstract syntax tree.
-castffi merge --inputDirectoryPath Packages/Dojo/Runtime/bindings/ast --outputFilePath Packages/Dojo/Runtime/bindings/ast/cross-platform.json
+castffi merge --inputDirectoryPath Bindings/ast --outputFilePath Bindings/ast/cross-platform.json
 
-./c2cs/artifacts/bin/C2CS.Tool/debug/C2CS.Tool generate --config Packages/Dojo/Runtime/bindings/config-generate-cs.json
+./c2cs/artifacts/bin/C2CS.Tool/debug/C2CS.Tool generate --config Bindings/config-generate-cs.json
