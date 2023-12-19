0948ed2d8993e77d0b17bb0a703c969790dc9250
Nasr <larko236@outlook.com>
2023-12-19 17:50:51 -0500

    feat: camera follow entities + show id

---
+++ Assets/Dojo/CameraFollow.cs
@@ -0,0 +1,47 @@
+using Dojo;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public WorldManager worldManager;
+    public float height = 10.0f; // height of the camera above the center point
+    public float damping = 1.0f; // damping factor to smooth camera movement
+    public float speed = 10.0f; // speed of camera movement
+
+
+    void Update()
+    {
+        if (worldManager.Entities().Length > 0)
+        {
+            Vector3 centerPoint = GetCenterPoint();
+            Vector3 offset = new Vector3(0, height, -height);
+            Vector3 desiredPosition = centerPoint + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, damping * Time.deltaTime);
+            transform.position = smoothedPosition;
+
+            transform.LookAt(centerPoint);
+        }
+
+        // Camera control
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (worldManager.Entities().Length == 1)
+        {
+            return worldManager.Entities()[0].transform.position;
+        }
+
+        var bounds = new Bounds(worldManager.Entities()[0].transform.position, Vector3.zero);
+        for (int i = 0; i < worldManager.Entities().Length; i++)
+        {
+            bounds.Encapsulate(worldManager.Entities()[i].transform.position);
+        }
+        return bounds.center;
+    }
+}
