ad54dd50d6acca6c2879ad0b95efc6d36c7e13aa
Nasr <larko236@outlook.com>
2025-01-30 17:48:33 +0700

    controller class

--- Assets/Dojo/Runtime/Controller.cs
+++ Assets/Dojo/Runtime/Controller.cs
@@ -3,27 +3,49 @@ using System.Threading.Tasks;
 using bottlenoselabs.C2CS.Runtime;
 using dojo_bindings;
 using UnityEngine;
+using Dojo.Starknet;
+using System.Linq;
 
-namespace Dojo.Starknet
+namespace Dojo
 {
+    public struct Policy
+    {
+        public FieldElement target;
+        public string method;
+        public string description;
+
+        public Policy(FieldElement target, string method, string description)
+        {
+            this.target = target;
+            this.method = method;
+            this.description = description;
+        }
+
+        public dojo.Policy ToNative()
+        {
+            return new dojo.Policy { target = target.Inner, method = method, description = description };
+        }
+    }
+
     public unsafe class Controller
     {
         private dojo.Controller* controller;
         public FieldElement Address => new FieldElement(dojo.controller_address(controller));
         public FieldElement ChainId => new FieldElement(dojo.controller_chain_id(controller));
-        public string Username => dojo.controller_username(controller);
+        public string Username => CString.ToString(dojo.controller_username(controller));
 
         private Controller(dojo.Controller* controller)
         {
             this.controller = controller;
         }
 
-        public static Controller? GetAccount(Policy[] policies, FieldElement chainId)
+        public static Controller GetAccount(Policy[] policies, FieldElement chainId)
         {
+            var nativePolicies = policies.Select(p => p.ToNative()).ToArray();
             dojo.Policy* policiesPtr = null;
-            if (policies.Length > 0)
+            if (nativePolicies.Length > 0)
             {
-                fixed (dojo.Policy* ptr = &policies[0])
+                fixed (dojo.Policy* ptr = &nativePolicies[0])
                 {
                     policiesPtr = ptr;
                 }
@@ -39,15 +61,16 @@ namespace Dojo.Starknet
             return new Controller(result._ok);
         }
 
-        public static async Task<Controller> Connect(string rpcUrl, Policy[] policies)
+        public static Task<Controller> Connect(string rpcUrl, Policy[] policies)
         {
+            var nativePolicies = policies.Select(p => p.ToNative()).ToArray();
             var connectionTask = new TaskCompletionSource<Controller>();
             CString crpcUrl = CString.FromString(rpcUrl);
 
             dojo.Policy* policiesPtr = null;
-            if (policies.Length > 0)
+            if (nativePolicies.Length > 0)
             {
-                fixed (dojo.Policy* ptr = &policies[0])
+                fixed (dojo.Policy* ptr = &nativePolicies[0])
                 {
                     policiesPtr = ptr;
                 }
@@ -61,15 +84,16 @@ namespace Dojo.Starknet
 
             dojo.controller_connect(crpcUrl, policiesPtr, (UIntPtr)policies.Length, onConnect);
 
-            return await connectionTask.Task;
+            return connectionTask.Task;
         }
 
         public static bool Clear(Policy[] policies, FieldElement chainId)
         {
+            var nativePolicies = policies.Select(p => p.ToNative()).ToArray();
             dojo.Policy* policiesPtr = null;
-            if (policies.Length > 0)
+            if (nativePolicies.Length > 0)
             {
-                fixed (dojo.Policy* ptr = &policies[0])
+                fixed (dojo.Policy* ptr = &nativePolicies[0])
                 {
                     policiesPtr = ptr;
                 }
